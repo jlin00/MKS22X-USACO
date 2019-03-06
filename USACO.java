56ced2f39ba4695a3e310e3c83610814a9422e35
Jackie Lin <jlin00@stuy.edu>
2019-03-06 10:13:32 -0500

    added code to store numbers from file in an array

--- USACO.java
+++ USACO.java
@@ -12,11 +12,32 @@ public class USACO{
       lines.add(info.nextLine());
     }
 
-    String[] firstline = lines.get(0).split(" ");
+    String[] firstline = lines.get(0).split(" "); //split the first line and store values
     int R = Integer.parseInt(firstline[0]);
     int C = Integer.parseInt(firstline[1]);
     int E = Integer.parseInt(firstline[2]);
     int N = Integer.parseInt(firstline[3]);
+
+    int[][] land = new int[R][C];
+    String[] temp; //temporary storage for line
+    for (int i = 1; i <= R; i++){
+      temp = lines.get(i).split(" "); //split the line
+      for (int j = 0; j < C; j++){
+        land[i-1][j] = Integer.parseInt(temp[j]); //add each number to array 
+      }
+    }
+
+    //print out land array for debugging purposes
+    /*
+    String output = "";
+    for (int r = 0; r < R; r++){
+      for (int c = 0; c < C; c++){
+        output += land[r][c] + " ";
+      }
+      output += "\n";
+    }
+    System.out.println(output);
+    */
 
     return 0;
   }
